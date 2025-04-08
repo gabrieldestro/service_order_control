09e483ca6783ce3f7399b00f94afb4a0a850b49c
gdestro <gabrielaugustodestro@gmail.com>
2025-04-07 21:49:26 -0300

    Exporting to excel

--- ServiceOrder/OrderListView.xaml.cs
+++ ServiceOrder/OrderListView.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Win32;
+using ServiceOrder.Domain.DTOs;
 using ServiceOrder.Domain.Entities;
-using ServiceOrder.DTOs;
 using ServiceOrder.Services.Interfaces;
 
 namespace ServiceOrder
@@ -15,16 +18,26 @@ namespace ServiceOrder
     {
         private readonly IOrderService _orderService;
         private readonly IOrderDeadlineService _orderDeadlineService;
+        private readonly IClientService _clientService;
+        private readonly IElectricCompanyService _electricCompanyService;
+        private readonly ISpreadsheetService _spreadsheetService;
         public ObservableCollection<OrderDTO> Orders { get; set; }
 
         public OrderListView(
             IOrderService orderService,
-            IOrderDeadlineService orderDeadlineService)
+            IOrderDeadlineService orderDeadlineService,
+            IClientService clientService,
+            IElectricCompanyService electricCompanyService,
+            ISpreadsheetService spreadsheetService)
         {
             InitializeComponent();
 
             _orderService = orderService;
             _orderDeadlineService = orderDeadlineService;
+            _clientService = clientService;
+            _electricCompanyService = electricCompanyService;
+            _spreadsheetService = spreadsheetService;
+
             Orders = new ObservableCollection<OrderDTO>();
             LoadOrdersAsync();
         }
@@ -35,6 +48,27 @@ namespace ServiceOrder
             detailView.SetOrder(null); // Nova ordem
             detailView.Closed += (s, args) => LoadOrdersAsync();
             detailView.ShowDialog();
+        }
+
+        private void OnExportClick(object sender, RoutedEventArgs e)
+        {
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Excel Files|*.xlsx",
+                Title = "Salvar como Excel",
+                FileName = $"Projetos_{DateTime.Now.Year}{DateTime.Now.Month}{DateTime.Now.Day}_{DateTime.Now.Hour}{DateTime.Now.Minute}{DateTime.Now.Second}.xlsx"
+            };
+
+            if (saveFileDialog.ShowDialog() != true)
+                return;
+
+            var memoryStream = _spreadsheetService.ExportOrdersToExcel(Orders.ToList());
+            using (var fileStream = File.Create(saveFileDialog.FileName))
+            {
+                memoryStream.CopyTo(fileStream);
+            }
+
+            MessageBox.Show("Arquivo salvo com sucesso!", "Aviso", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void OnNextClick(object sender, RoutedEventArgs e)
@@ -93,6 +127,9 @@ namespace ServiceOrder
                 var orders = await Task.Run(() => _orderService.GetAllAsync());
                 var filteredOrders = filter != null ? orders.Where(filter) : orders;
 
+                var clients = await Task.Run(() => _clientService.GetAllAsync());
+                var electricCompanies = await Task.Run(() => _electricCompanyService.GetAllAsync());
+
                 var deadlines = await Task.Run(() => _orderDeadlineService.GetAllAsync());
                 var generalDeadline = deadlines.FirstOrDefault(d => String.IsNullOrEmpty(d.OrderId));
                 
@@ -100,6 +137,10 @@ namespace ServiceOrder
                 {
                     var specificDeadline = deadlines.FirstOrDefault(d => d.OrderId == order.OrderName.ToString());
                     var deadline = specificDeadline ?? generalDeadline;
+
+                    order.Client = clients.FirstOrDefault(c => c.Id == order.ClientId);
+                    order.FinalClient = clients.FirstOrDefault(c => c.Id == order.FinalClientId);
+                    order.ElectricCompany = electricCompanies.FirstOrDefault(c => c.Id == order.ElectricCompanyId);
 
                     var dto = new OrderDTO
                     {
@@ -128,6 +169,7 @@ namespace ServiceOrder
             ClearButton.IsEnabled = show;
             NewRegistrationButton.IsEnabled = show;
             NextButton.IsEnabled = show;
+            ExportButton.IsEnabled = show;
 
             LoadingProgressBar.Visibility = show ? Visibility.Collapsed : Visibility.Visible;
         }
